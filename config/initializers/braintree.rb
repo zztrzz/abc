17ed3a6f6e7b4a127307909b1e8fd8d7606099c1
zztrzz <zorluhan@gmail.com>
2014-08-02 12:15:47 +0300

    removed stripe

--- config/initializers/braintree.rb
+++
@@ -1,4 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "use_your_merchant_id"
-Braintree::Configuration.public_key = "use_your_public_key"
-Braintree::Configuration.private_key = "use_your_private_key"
